84f810a00f21eb15e955aa889740c31dcacf1eee
IT21162046 <99236417+IT21162046@users.noreply.github.com>
2023-05-08 01:37:27 +0530

    changes

--- crudadmin/src/main/java/com/example/crudadmin/PackageData.kt
+++ crudadmin/src/main/java/com/example/crudadmin/PackageData.kt
@@ -1,6 +1,6 @@
 package com.example.crudadmin
 
 data class PackageData(val name : String? = null, val description : String? = null, val price : String? = null,
-                       val rating : String? = null, val number : String? = null){
+                       val duration : String? = null, val route : String? = null, val way : String? = null, val accommodation : String? = null, val number : String? = null){
 
 }
