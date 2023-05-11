e8c93b548c32933ea9d6e40a31678b800dcc3247
IT21162046 <99236417+IT21162046@users.noreply.github.com>
2023-05-11 13:25:59 +0530

    changes

--- app/src/main/java/com/example/tourpack/MainActivity.kt
+++ app/src/main/java/com/example/tourpack/MainActivity.kt
@@ -18,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         replaceFragment(Home())
 
         binding.bottomNavigationView.setOnItemSelectedListener {
@@ -47,5 +46,9 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
 
     }
+
+}
+
+class ActivityMainBinding {
 
 }
--- crudadmin/src/main/java/com/example/crudadmin/Update.kt
+++ crudadmin/src/main/java/com/example/crudadmin/Update.kt
@@ -2,10 +2,60 @@ package com.example.crudadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.crudadmin.databinding.ActivityUpdateBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Update : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener {
+            val uploadNumber = binding.uploadNumber.text.toString()
+            val uploadName = binding.uploadName.text.toString()
+            val uploadDescription = binding.uploadDescription.text.toString()
+            val uploadPrice = binding.uploadPrice.text.toString()
+            val uploadDuration = binding.uploadDuration.text.toString()
+            val uploadRoute = binding.uploadRoute.text.toString()
+            val uploadWay = binding.uploadWay.text.toString()
+            val uploadAccommodation = binding.uploadAccommodation.text.toString()
+            updateData(uploadNumber,uploadName,uploadDescription,uploadPrice,uploadDuration,uploadRoute,uploadWay,uploadAccommodation)
+        }
+
     }
+
+    private fun updateData(number: String, name: String, description: String, price: String, duration: String, route: String, way: String, accommodation: String){
+        database = FirebaseDatabase.getInstance().getReference("Packages")
+        val packagedata = mapOf<String,String>(
+            "name" to name,
+            "description" to description,
+            "price" to price,
+            "duration" to duration,
+            "route" to route,
+            "way" to way,
+            "accommodation" to accommodation
+        )
+
+        database.child(number).updateChildren(packagedata).addOnSuccessListener {
+            binding.uploadNumber.text.clear()
+            binding.uploadName.text.clear()
+            binding.uploadDescription.text.clear()
+            binding.uploadPrice.text.clear()
+            binding.uploadDuration.text.clear()
+            binding.uploadRoute.text.clear()
+            binding.uploadWay.text.clear()
+            binding.uploadAccommodation.text.clear()
+            Toast.makeText(this,"Successfully Updated",Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(this,"Failed to Update",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
